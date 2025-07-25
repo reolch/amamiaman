1468e9ce19f1ad8c384da7bbfc0923c6b04ff35a
MSI\admin <admin@test.com>
2025-07-25 23:59:59 +0900

    HeaderとFooterのデザインを変更しました。

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -1,11 +1,6 @@
-"use client";
 import styles from './Footer.module.css';
 
 const Footer = () => {
-    const scrollToTop = (e) => {
-        e.preventDefault();
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
 
     return (
         <footer className={styles.footer} role="contentinfo">
@@ -25,17 +20,6 @@ const Footer = () => {
                         Copyright © ヤマハタマリンサービスあまん All Rights Reserved.
                     </p>
                 </div>
-            </div>
-            <div className={styles.footer__backToTop}>
-                <a
-                    href="#top"
-                    onClick={scrollToTop}
-                    className={styles.footer__backToTopLink}
-                    aria-label="ページのトップへ戻る"
-                    role="button"
-                >
-                    ↑ Top
-                </a>
             </div>
         </footer>
     );
